0968bb089658c0b0aab6ca83ceb38e975ae8ead2
celikyucel <clkyucel3@gmail.com>
2020-10-13 18:47:04 +0200

    Feature2 more added

--- src/test/java/feature2.java
+++ src/test/java/feature2.java
@@ -3,5 +3,7 @@ public class feature2 {
     public static void main(String[] args) {
 
         System.out.println("Working on Feature2 ");
+        System.out.println("Working on Feature2 more ");
+        System.out.println("Working on Feature2 again ");
     }
 }
