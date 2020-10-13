31ac8fb522f8b0d1a21ea4104e9102f5b3a27b24
celikyucel <clkyucel3@gmail.com>
2020-10-13 18:57:03 +0200

    adding one more changing to line number 3

--- src/test/java/feature2.java
+++ src/test/java/feature2.java
@@ -5,5 +5,7 @@ public class feature2 {
         System.out.println("Working on Feature2 ");
         System.out.println("Working on Feature2 more ");
         System.out.println("Working on Feature2 again ");
+
+        System.out.println("adding one more changing to line number 3");
     }
 }
